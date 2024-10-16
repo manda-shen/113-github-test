6f1ab74a331b975e09a206223ec4e8183fe55be3
Manda <mandashen@gmail.com>
2024-10-17 00:49:46 +0800

    array增加自己練習，及增加了table.html來參考表格用語

--- array.php
+++ array.php
@@ -96,5 +96,78 @@ $hebe=['hebe', 71, 62, 80, 62, 64];
     </tr>
 
 
+    <style>
+table{
+    border-collapse: collapse;
+    margin:20px;
+
+}
+table td, table th{
+    border:1px solid #aaa;
+    width:60px;
+    height:30px;
+    text-align: center;
+}
+table tr:nth-child(1){
+    background:#325335;
+    color:white
+}
+table td:nth-child(1){
+    background:#325335;
+    color:white
+}
+
+
+</style>
+<?php
+
+/*
+$sub=['科目', '國文', '英文', '數學', '地理', '歷史'];
+$student=['judy','amo','john','peter','hebe'];
+$judy=[95, 64, 70, 90, 84];
+$amo=[88, 78, 54, 81, 71];
+$john=[45, 60, 68, 70, 62];
+$peter=[59, 32, 77, 54, 42];
+$hebe=[71, 62, 80, 62, 64];
+*/
+
+$sub=['科目', '國文', '英文', '數學', '地理', '歷史'];
+$score=[];
+$score[]=['judy', 95, 64, 70, 90, 84];
+$score[]=['amo', 88, 78, 54, 81, 71];
+$score[]=['john', 45, 60, 68, 70, 62];
+$score[]=['peter', 59, 32, 77, 54, 42];
+$score[]=['hebe', 71, 62, 80, 62, 64];
+
+
+echo "<table>";
+echo "<tr>";
+foreach($sub as $subject){
+    echo "<th>$subject</th>";
+}
+echo "</tr>";
+
+foreach($score as $student){
+    echo "<tr>";
+    foreach($student as $data){
+        echo "<td>$data</td>";
+    }
+    echo "</tr>";
+}
+
+echo "</table>";
+
+
+
+
+?>
+
+<table></table>
+<tr>
+    <td></td>
+</tr>
+
+
+
 </body>
 </html>
